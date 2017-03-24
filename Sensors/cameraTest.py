c4bba273432108fb9ffd44ddd045468215829f85
Dillon MacEwan <dillon@mekazoic.com>
2017-03-24 21:08:46 +1100

    Update cameraTest.py

--- Sensors/cameraTest.py
+++ Sensors/cameraTest.py
@@ -1,3 +1,13 @@
 # uses the python-picamera library
 
 import picamera
+import time
+
+#take a photo
+camera = picamera.PiCamera()
+camera.capture('image.jpg')
+
+#make a 5s movie
+camera.start_recording('video.h264')
+time.sleep(5)
+camera.stop_recording()
