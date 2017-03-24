6ad75dd591c1a2253c0fd73e2140d30bd93d185d
Dillon MacEwan <dillon@mekazoic.com>
2017-03-24 20:32:24 +1100

    Update and rename barometerBMP180P.py to Sensors/barometerBMP180P.py

---
+++ Sensors/barometerBMP180P.py
@@ -0,0 +1,14 @@
+# sample script for barometer
+# uses Adafruit BMP085 and I2C libraries
+
+from Adafruit_BMP085 import BMP085
+
+bmp = BMP085(0x77)
+
+temp = bmp.readTemperature()
+pressure = bmp.readPressure()
+altitude = bmp.readAltitude()
+
+print "Temperature: %.2f C" %temp
+print "Pressure: %.2f hPa" %(pressure/100.0)
+print "Altitude: %.2f" %taltitude
