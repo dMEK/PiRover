af9b5b20bf46d4f0e43e5ff432a2ba35e58e8076
dMEK <dillon@mekazoic.com>
2017-03-24 18:27:02 +1100

    Create magSense.py

---
+++ Sensors/magSense.py
@@ -0,0 +1,19 @@
+# simple sample code for a simple hall effct switch sensor
+# this type of sensor either outputs a high or a low sig depending 
+# on the presence of a magnetic field
+#  pull_up_down = GPIO.PUD_DOWN sets the input pin to low when no input
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(false)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+prev_input = 0
+while True:
+  input = GPIO.input(11)
+  if ((not prev_input) and input):
+    print "Field changed."
+  prev_input = input
+  time_sleep(0.05)
+  
