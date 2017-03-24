642cf9b01a37c6aee6055dd2e840bf306451c0b9
Dillon MacEwan <dillon@mekazoic.com>
2017-03-24 19:05:03 +1100

    Update magSense.py

--- Sensors/magSense.py
+++ Sensors/magSense.py
@@ -6,7 +6,7 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setwarnings(false)
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 prev_input = 0
