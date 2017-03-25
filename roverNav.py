0da41b004a76cf4b8c48033943ea2a5fa973d1f0
Dillon MacEwan <dillon@mekazoic.com>
2017-03-25 21:43:02 +1100

    Create roverNav.py

---
+++ roverNav.py
@@ -0,0 +1,8 @@
+# This is the basic script described in the Make book
+# The code has no Autonomy, it just takes instruction from and 
+# transmits data to the user
+# the sections of this code are based on the scripts in Motors and Sensors
+# And should be suitabel foundation pieces for autonomy
+
+import time
+import os # python os module allows for interface with underying os on pi
